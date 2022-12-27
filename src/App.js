611d85fdfd4665eaca3d903f956c40fc15356d84
mj9457 <mj9457@naver.com>
2022-12-27 23:08:13 +0900

    feat.Navbar에 icon 추가

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 import NavbarPages from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'remixicon/fonts/remixicon.css';
 
 function App() {
   return (
-    <div className="App" style={{ height: '7000px' }}>
+    <div className="App">
       <NavbarPages />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,26 +2,34 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavbarPages() {
   return (
     <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+        fixed="top"
+        style={{ padding: '0' }}
+      >
         <Container>
-          <Navbar.Brand href="#home">로고</Navbar.Brand>
+          <Navbar.Brand href="#home" style={{ padding: '0' }}>
+            <i class="ri-question-mark ri-2x"></i>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#list">길드 구직 리스트</Nav.Link>
-              <Nav.Link href="#form">길드 구인 신청</Nav.Link>
+              <Nav.Link href="#list">🔸길드구직리스트</Nav.Link>
+              <Nav.Link href="#form">🔹길드구인신청</Nav.Link>
             </Nav>
             <Nav>
               <Nav.Link href="https://open.kakao.com/o/sAJp6DVe">
-                카카오톡 아이콘
+                <i class="ri-kakao-talk-fill ri-2x"></i>
               </Nav.Link>
               <Nav.Link href="https://github.com/mj9457/lostark-guild-filtering">
-                깃허브 아이콘
+                <i class="ri-github-fill ri-2x"></i>
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
