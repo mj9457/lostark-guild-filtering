97860baf8201c314463b3446deabd93de9d4e692
mj9457 <mj9457@naver.com>
2022-12-27 23:35:16 +0900

    feat. 페이지 route 구조화

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './App.css';
-import NavbarPages from './components/Navbar';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'remixicon/fonts/remixicon.css';
+
+import { Routes, Route } from 'react-router-dom';
+
+import NavbarPages from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <NavbarPages />
+
+      <Routes>
+        {/* main page */}
+        <Route path="/" element={<div>메인페이지</div>} />
+        <Route path="/main" element={<div>메인페이지</div>} />
+
+        {/* list page */}
+        <Route path="/list" element={<div>길드구직리스트</div>} />
+
+        {/* form page */}
+        <Route path="/form" element={<div>길드구인신청폼</div>} />
+
+        {/* 404 page */}
+        <Route path="*" element={<div>없는 페이지</div>} />
+      </Routes>
     </div>
   );
 }
